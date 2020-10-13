d100c028f426ce85505109c54e413a730e80efce
vivekm <viv.mttl@gmail.com>
2020-10-13 09:57:50 +0530

    [binary_search] : Add CountElementOccurence.java

--- binary_search/CountElementOccurence.java
+++ binary_search/CountElementOccurence.java
@@ -1,13 +1,15 @@
-public class Solution {
+import java.util.*;
 
-    public boolean compare(int val, int target, boolean lower) {
+public class CountElementOccurence {
+
+    public static boolean compare(int val, int target, boolean lower) {
 
         if (lower) return val < target;
         return val <= target;
     }
 
     // DO NOT MODIFY THE LIST. IT IS READ ONLY
-    public int num_occurences(final List<Integer> arr, int target, boolean lower) {
+    public static int num_occurences(final List<Integer> arr, int target, boolean lower) {
 
         int l = 0, r = arr.size();
         while (l < r) {
@@ -19,8 +21,23 @@ public class Solution {
         return l - 1;
     }
 
-    public int findCount(final List<Integer> A, int B) {
+    public static int findCount(final List<Integer> A, int B) {
 
         return num_occurences(A, B, false) - num_occurences(A, B, true);
+    }
+
+    public static void main(String[] args) {
+
+      List<Integer> arr = new ArrayList<>(Arrays.asList(5, 7, 7, 8, 8, 10));
+
+      int res = findCount(arr, 7);
+      System.out.println(res);
+
+      int res1 = findCount(arr, 4);
+      System.out.println(res1);
+
+      int res2 = findCount(arr, 12);
+      System.out.println(res2);
+
     }
 }
